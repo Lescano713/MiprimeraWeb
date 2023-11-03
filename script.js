6f59509ae6336ce01e36629d19a275a95eecfdfc
Lescano713 <131562199+Lescano713@users.noreply.github.com>
2023-11-02 20:10:57 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var alerta = document.getElementById("form");
+var name = document.getElementById("name");
+var parrafo = document.getElementById("alertas");
+
+// //alert("hola este es mi javascript")
+// /*let nombre = "Danae" ;
+// var nombre2= "Daniella";
+// const nombre3 ="gato";
+// console.log(nombre)
+// console.log(nombre2)
+// console.log(nombre3)*/
+// //ejemplo
+// let contenidoTitulo = "Nombres";
+// let titulo = document.querySelector(".titulo1");
+// //let titulo = document.querySelector(.logo .fuente)
+// titulo.innerHTML = contenidoTitulo;
+// let textotitulo = titulo.innerText;
+// console.log(textotitulo);
+// if(textotitulo == "Nombre"){
+//     titulo.innerHTML = "Mi web"
+// } else{
+//     console.log("error en el sistema")
+// }
+
+// let nombre;
+// let gusto;
+// let ocupacion;
+
+// let parrafo = document.querySelector(".acerca-de")
+
+// function cambiarpalabras(nombre,gusto,ocupacion){
+//     let contenido =`Bienvenido a mi página de cosas, aqui aprenderas más sobre mi. Mi nombre es ${nombre}, nací en Perú, actualmente resido en Lima, me
+//     gusta ${gusto} y dormir con mis gatos. Soy una persona apasionada por
+//     la ${ocupacion}. Para mí, la programación es mucho más que solo
+//     escribir código; es el lenguaje con el que puedo dar vida a mis
+//     ideas y resolver problemas de manera creativa. Disfruto
+//     enfrentando desafíos técnicos, ya que los veo como oportunidades
+//     para crecer y aprender constantemente`
+//     return contenido;
+// }
+
+// parrafo.innerText = cambiarpalabras("danae yazuri lescano"," pintar, la jardineria","ingieneria y programacion");
+
